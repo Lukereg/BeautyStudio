635bc2fcd314603ba2c6b043a1de8338d6b4d74d
Łukasz Krzysztoń <79262330+Lukereg@users.noreply.github.com>
2023-03-15 16:50:17 +0100

    Add project files.

---
+++ BeautyStudio.MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace BeautyStudio.MVC.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
